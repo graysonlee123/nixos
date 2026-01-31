4eb43c29ec149dd2c8c0d775c83dd455d4961bbd
Grayson Gantek <github@graysn.com>
2026-01-30 20:01:46 -0500

    Remove unnecessary comments

--- hosts/nostromo/configuration.nix
+++ hosts/nostromo/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running 'nixos-help').
-
 { config, pkgs, inputs, ... }:
 
 let
@@ -30,28 +26,12 @@ in
   nixpkgs.config.allowUnfree = true; # Needed for NVIDIA
   system.stateVersion = "25.11";
   greeter.unsupportedGpu = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Galaxy 70 keyboard - function keys fix
   boot.extraModprobeConfig = ''
     options hid_apple fnmode=2
   '';
-
-  # Enable flakes and nix-command
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Home Manager
   home-manager.users.gray = { config, lib, pkgs, ... }: {
