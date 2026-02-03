c0e318cb0d958d5429bc94ae13890483d403f238
Grayson Gantek <github@graysn.com>
2026-02-03 10:33:37 -0500

    Make sure to use 25.11 of home-manager, nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
