5fffdeb1dec7a1b65ef62e6b754b07596f096cac
Grayson Gantek <github@graysn.com>
2026-02-16 16:58:55 -0500

    Add additional yazi bookmarks

--- modules/home-manager/yazi.nix
+++ modules/home-manager/yazi.nix
@@ -7,6 +7,14 @@
           show_hidden = true;
         };
       };
+      keymap = {
+        mgr.prepend_keymap = [
+          { on = [ "g" "d" ]; run = "cd ~/downloads"; desc = "Go ~/downloads"; }
+          { on = [ "g" "i" ]; run = "cd ~/repos/inspry"; desc = "Go ~/repos/inspry"; }
+          { on = [ "g" "m" ]; run = "cd ~/repos/me/"; desc = "Go ~/repos/me"; }
+        ];
+      };
     };
   };
 }
+
