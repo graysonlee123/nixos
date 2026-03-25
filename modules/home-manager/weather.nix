647d421cbc4e0b6a7d38c4c31aa26efec39aa1d8
Grayson Gantek <github@graysn.com>
2026-03-25 14:31:37 -0400

    Switch weather to fahrenheit

--- modules/home-manager/weather.nix
+++ modules/home-manager/weather.nix
@@ -4,11 +4,11 @@ let
   toggleScript = pkgs.writeShellScript "weather" ''
     for i in {1..5}
     do
-      text=$(${pkgs.curl}/bin/curl -s "https://wttr.in/$1?format=1")
+      text=$(${pkgs.curl}/bin/curl -s "https://wttr.in/$1?format=1&u")
       if [[ $? == 0 ]]
       then
         text=$(echo "$text" | ${pkgs.gnused}/bin/sed -E "s/\s+/ /g")
-        tooltip=$(${pkgs.curl}/bin/curl -s "https://wttr.in/$1?format=4")
+        tooltip=$(${pkgs.curl}/bin/curl -s "https://wttr.in/$1?format=4&u")
         if [[ $? == 0 ]]
         then
           tooltip=$(echo "$tooltip" | ${pkgs.gnused}/bin/sed -E "s/\s+/ /g")
