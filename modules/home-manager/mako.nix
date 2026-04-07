5fa17499ef1c2d2dc1e4966a9e1f1216e651a250
Grayson Gantek <github@graysn.com>
2026-04-07 18:30:04 -0400

    Add basic mako notification criterias

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -1,4 +1,6 @@
-{
+let
+  timeout = 5000;
+in {
   config = {
     services.mako = {
       enable = true;
@@ -6,8 +8,26 @@
         border-size = 2;
         width = 400;
         height = 150;
-        ignore-timeout = false;
+        ignore-timeout = true;
         max-visible = 5;
+
+        # Criteria
+        #
+        # See https://github.com/emersion/mako/blob/master/doc/mako.5.scd#criteria
+        #
+        # Useful commands:
+        # - makoctl list (show currently displayed notifications)
+        # - makoctl history (show dismissed notifications)
+        "app-name=\"tidal-hifi\"" = {
+          default-timeout = timeout;
+          anchor = "top-center";
+          icon-location = "top";
+          text-alignment = "left";
+          width = 256;
+        };
+        "desktop-entry=\"com.mitchellh.ghostty\"" = {
+          default-timeout = timeout;
+        };
       };
     };
   };
