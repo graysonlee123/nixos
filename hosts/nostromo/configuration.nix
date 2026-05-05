f640db13aabf9f31309559d3645a1072cdc79525
Grayson Gantek <github@graysn.com>
2026-05-05 19:38:26 -0400

    feat: add ext4 SSD to fstab on Nostromo

--- hosts/nostromo/configuration.nix
+++ hosts/nostromo/configuration.nix
@@ -12,5 +12,11 @@
   host.name = "nostromo";
   system.stateVersion = "25.11";
   greeter.unsupportedGpu = true;
+
+  fileSystems."/mnt/storage" = {
+    device = "/dev/disk/by-uuid/983d9ad7-1cbc-490d-8bd4-c10704a634f4";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
 }
 
