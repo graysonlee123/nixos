3a097b343c4ea1f323ec6072663576ff80d6393a
Grayson Gantek <github@graysn.com>
2026-05-07 22:36:05 -0400

    fix(fingerprint): remove ly greeter; disable fingerprint login
    Fingerprint login provides password-less login, which can't decrypt
    gnome-keyring, causing additional unlock prompts. Decided to force
    password login, because I'm going to have to type it anyways to properly
    dismiss these prompts.

--- modules/nixos/fingerprint.nix
+++ modules/nixos/fingerprint.nix
@@ -1,10 +1,8 @@
-{ lib, ... }: {
+{ lib, ... }:
+
+{
   services.fprintd.enable = true;
-  services.greetd.enable = lib.mkForce false;
-  services.displayManager.ly.enable = true;
-  security.pam.services = {
-    login.fprintAuth = true;
-    sudo.fprintAuth = true;
-    ly.fprintAuth = true;
-  };
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.greetd.fprintAuth = false;
 }
+
--- modules/nixos/keyring.nix
+++ modules/nixos/keyring.nix
@@ -2,5 +2,6 @@
   config = {
     services.gnome.gnome-keyring.enable = true;
     security.pam.services.login.enableGnomeKeyring = true;
+    security.pam.services.greetd.enableGnomeKeyring = true;
   };
 }
