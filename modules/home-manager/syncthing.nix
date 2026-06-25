050441b0ff25d392d943038410b2a929b040f14c
Grayson Gantek <github@graysn.com>
2026-06-25 11:59:24 -0400

    Revert "fix(syncthing): add explicit syncthing port"
    This reverts commit 793171317413bd9e77bebd94ad126d7f98069ab4.

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -5,22 +5,22 @@ let
     nostromo = {
       id = "5N4LBAT-Q3W7TMI-4QG2Y66-3HUGMR7-PDL7WBJ-OHOBREZ-5P6LHT5-4BRVSQ7";
       addresses = [
-        "tcp://100.73.78.2:22000"
-        "tcp://192.168.86.38:22000"
+        "tcp://100.73.78.2"
+        "tcp://192.168.86.38"
       ];
     };
     corbelan = {
       id = "LQURRGV-4URVSOT-S2QMDXK-I2KETJD-POFM5CX-4KNHSGR-X2AAV44-5RYCJQJ";
       addresses = [
-        "tcp://100.75.203.122:22000"
-        "tcp://192.168.86.42:22000"
+        "tcp://100.75.203.122"
+        "tcp://192.168.86.42"
       ];
     };
     sulaco = {
       id = "E52YS3K-QNNKP2Z-R5LMP6E-7WSRDOC-KMPUGR2-Z4KHKK3-TH2XUBY-6VGJ4AI";
       addresses = [
-        "tcp://100.93.40.89:22000"
-        "tcp://192.168.86.2:22000"
+        "tcp://100.93.40.89"
+        "tcp://192.168.86.2"
       ];
     };
   };
