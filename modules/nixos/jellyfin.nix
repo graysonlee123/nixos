ef704ee7c2d616fc7c3e10dc703fc5f14c02f6c5
Grayson Gantek <github@graysn.com>
2026-06-27 17:15:26 -0400

    feat(sulaco): add group pinchflat to jellyfin user so it can access pinchflat media

--- modules/nixos/jellyfin.nix
+++ modules/nixos/jellyfin.nix
@@ -1,4 +1,7 @@
+{ config, lib, ... }:
+
 {
+  users.users.jellyfin.extraGroups = lib.optional config.services.pinchflat.enable config.services.pinchflat.user;
   services.jellyfin = {
     enable = true;
     openFirewall = true;
