806c8a2b7168f94537f8f7173d15f712c09778ae
Grayson Gantek <github@graysn.com>
2026-06-27 13:22:47 -0400

    feat(postgres): allow connections from docker0

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_17;
+    settings = {
+      listen_addresses = lib.mkForce "localhost,172.17.0.1";
+    };
+    authentication = lib.mkAfter ''
+      host all all 172.17.0.0/16 md5
+    '';
     ensureDatabases = [
       "vikunja"
       "linkding"
