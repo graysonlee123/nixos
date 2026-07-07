98a99c7aee2e75ea104b0020549939ffa51fd91a
Grayson Gantek <github@graysn.com>
2026-07-07 19:54:34 -0400

    feat(xdg): add userDirs config for lowercase user directories

--- modules/home-manager/xdg.nix
+++ modules/home-manager/xdg.nix
@@ -1,3 +1,5 @@
+{ config, ... }:
+
 {
   xdg.desktopEntries.yazi-open = {
     name = "Yazi";
@@ -46,5 +48,17 @@
     "text/x-shellscript" = "vim.desktop";
     "application/json" = "vim.desktop";
     "application/xml" = "vim.desktop";
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${config.home.homeDirectory}/desktop";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/downloads";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/pictures";
+    publicShare = "${config.home.homeDirectory}/public";
+    templates = "${config.home.homeDirectory}/templates";
+    videos = "${config.home.homeDirectory}/videos";
   };
 }
