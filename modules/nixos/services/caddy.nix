27b1cf086b62d79d9f25171f958c932b30daec8e
Grayson Gantek <github@graysn.com>
2026-08-23 04:26:00 -0400

    chore(caddy): update caddy-dns/cloudflare hash

--- modules/nixos/services/caddy.nix
+++ modules/nixos/services/caddy.nix
@@ -13,7 +13,7 @@ in
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "sha256-4WF7tIx8d6O/Bd0q9GhMch8lS3nlR5N3Zg4ApA3hrKw=";
+      hash = "sha256-PWadA5qr/gR2qDcT8l8u1Xku7LM2HIfWTLOkzezCYy0==";
     };
     environmentFile = config.sops.templates."caddy.env".path;
     globalConfig = ''
