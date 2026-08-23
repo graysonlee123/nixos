0fee7ea84058ea118510debd23c7c5e54c472e38
Grayson Gantek <github@graysn.com>
2026-08-23 05:10:55 -0400

    feat(stylix): change theme to Penumbra Dark Contrast Plus; add useful comments/links

--- modules/nixos/theme/stylix.nix
+++ modules/nixos/theme/stylix.nix
@@ -10,13 +10,19 @@
     # Even on headless, requires a wallpaper
     image = ../../../assets/images/river.jpg;
 
+    # Gallery: https://tinted-theming.github.io/tinted-gallery/
+    # Available themes: https://github.com/tinted-theming/schemes/tree/43dd14f6466a782bd57419fdfb5f398c74d6ac53/base16
+    # The available themes are built from that specific revision of tinted-theming/schemes in https://github.com/NixOS/nixpkgs/blob/release-26.05/pkgs/by-name/ba/base16-schemes/package.nix
+
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-dune.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/horizon-terminal-dark.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/jabuti.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/lime.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/tarot.yaml";
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/caroline.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/penumbra-dark-contrast-plus.yaml";
 
     fonts = {
       serif = {
