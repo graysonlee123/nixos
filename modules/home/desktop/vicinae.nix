d092220246f65eb38335cfcb0a7050c7ce9719c1
Grayson Gantek <github@graysn.com>
2026-09-05 01:40:12 -0400

    feat(vicinae): add gtoolbox extension

--- modules/home/desktop/vicinae.nix
+++ modules/home/desktop/vicinae.nix
@@ -1,3 +1,4 @@
+# vicinae.mkExtension def: https://github.com/nix-community/home-manager/blob/693e8ce0fb240a73c116a03cfd7b19269c87af88/modules/programs/vicinae/lib.nix
 {
   config,
   pkgs,
@@ -23,6 +24,15 @@
             sha256 = "sha256-j3g10f7sHHPbcN6tQIJmKatyOANJzHc5o9zAQlNrnOw=";
           }
           + /extensions/nix;
+      })
+      (config.lib.vicinae.mkExtension {
+        name = "gtoolbox";
+        src = pkgs.fetchFromGitHub {
+          owner = "graysonlee123";
+          repo = "gtoolbox";
+          rev = "99e23f9a7c84da41486d1b1fc70b8ef8dba6a4c5";
+          sha256 = "sha256-/dB7cr3MUEBa1EWaWTln5v1Nq7dF6x5nNDZM6TLagkc=";
+        };
       })
     ];
     systemd = {
